2b5f717a93d6c1009a52575f22d282eded92ba1c
Diwoop <144156230+Diwoop@users.noreply.github.com>
2024-06-26 18:35:13 +0000

    excuse generator en la carpeta que toca

--- src/app.js
+++ src/app.js
@@ -1,11 +1,33 @@
-/* eslint-disable */
-import "bootstrap";
-import "./style.css";
+let opcionesQuien = [
+  "El perro",
+  "Mi gato",
+  "Mi abuela",
+  "El repartidor",
+  "Mi madre"
+];
+let opcionesAccion = [
+  "se ha comido",
+  "se ha meado en",
+  "ha roto",
+  "se ha llevado"
+];
+let opcionesQue = ["mis deberes", "mi movil", "mi coche", "mis llaves"];
+let opcionesCuando = [
+  "antes de clase",
+  "mientras yo dormia",
+  "mientras estaba en el gimnasio",
+  "cuando estaba rezando"
+];
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+let quien = Math.floor(Math.random() * opcionesQuien.length);
+let accion = Math.floor(Math.random() * opcionesAccion.length);
+let que = Math.floor(Math.random() * opcionesQue.length);
+let cuando = Math.floor(Math.random() * opcionesCuando.length);
+
+function excusaFinal() {
+  return `${opcionesQuien[quien]} ${opcionesAccion[accion]} ${opcionesQue[que]} ${opcionesCuando[cuando]}!!`;
+}
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  document.getElementById("excusa").innerHTML = excusaFinal();
 };
